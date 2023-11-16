159de765f3eceac4c1d249f689919d5d8ed80d21
Nathaniel Canilang <nathcanilang@gmail.com>
2023-11-17 07:47:20 +0800

    Fixed the void button

--- CafeSystem/Microsoft/VisualBasic/Interaction.cs
+++ CafeSystem/Microsoft/VisualBasic/Interaction.cs
@@ -1,12 +1,44 @@
 ﻿using System;
+using System.Windows.Forms;
 
 namespace Microsoft.VisualBasic
 {
     internal class Interaction
     {
-        internal static string InputBox(string v1, string v2, string v3)
+        internal static string InputBox(string prompt, string title, string defaultResponse)
         {
-            throw new NotImplementedException();
+            using (Form inputForm = new Form())
+            using (TextBox textBox = new TextBox())
+            using (Button okButton = new Button())
+            using (Label label = new Label())
+            {
+                inputForm.Text = title;
+                inputForm.Size = new System.Drawing.Size(300, 150);
+                inputForm.FormBorderStyle = FormBorderStyle.FixedSingle;
+                inputForm.StartPosition = FormStartPosition.CenterScreen;
+
+                label.Text = prompt;
+                label.Size = new System.Drawing.Size(200, 20);
+                label.Location = new System.Drawing.Point(50, 10);
+
+                textBox.Size = new System.Drawing.Size(200, 20);
+                textBox.Location = new System.Drawing.Point(50, 30);
+                textBox.Text = defaultResponse;
+
+                okButton.DialogResult = DialogResult.OK;
+                okButton.Name = "okButton";
+                okButton.Size = new System.Drawing.Size(75, 23);
+                okButton.Location = new System.Drawing.Point(50, 70);
+                okButton.Text = "OK";
+
+                inputForm.Controls.AddRange(new Control[] { label, textBox, okButton });
+
+                inputForm.AcceptButton = okButton;
+
+                DialogResult result = inputForm.ShowDialog();
+
+                return result == DialogResult.OK ? textBox.Text : string.Empty;
+            }
         }
     }
 }
